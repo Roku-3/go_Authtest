0657cc26aa22117ffe2bd56aef576233c2d16a0f
roku3 <down01134@gmail.com>
2020-12-10 10:37:09 +0900

    first commit

---
+++ src/templates/login.go
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>
+            login page
+        </title>
+        <link rel="stylesheet"
+              href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="panel panel-success">
+                <div class="panel-title">
+                    <h2>Auth Page</h2>
+                </div>
+                <a href="/auth/login/google">Google</a>
+            </div>
+        </div>
+    </body>
+</html>
